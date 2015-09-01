78005078b5c23b5b0fc0d58978846a5f0c8240d5
Ravi <ravi@neevtech.com>
2015-09-01 19:00:07 +0530

    Changes for styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -91,7 +91,7 @@ module.exports = function (grunt) {
         proxies: [{
           context: '/api',
           host: 'dev.hillromvest.com',
-          port: 80
+          port: 8080
         }]
       },
 
@@ -200,15 +200,15 @@ module.exports = function (grunt) {
     compass: {
       options: {
         sassDir: '<%= yeoman.app %>/styles',
-        cssDir: '.tmp/styles',
-        generatedImagesDir: '.tmp/images/generated',
+        cssDir: '<%= yeoman.app %>/styles',
+        generatedImagesDir: '<%= yeoman.app %>/images/generated',
         imagesDir: '<%= yeoman.app %>/images',
         javascriptsDir: '<%= yeoman.app %>/scripts',
-        fontsDir: '<%= yeoman.app %>/styles/fonts',
-        importPath: './bower_components',
+        fontsDir: '<%= yeoman.app %>/fonts',
+        importPath: '<%= yeoman.app %>/bower_components',
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
-        httpFontsPath: '/styles/fonts',
+        httpFontsPath: '/fonts',
         relativeAssets: false,
         assetCacheBuster: false,
         raw: 'Sass::Script::Number.precision = 10\n'
