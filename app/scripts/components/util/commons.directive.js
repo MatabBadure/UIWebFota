145fd0856f405e029bb5a16040b6ef7ebc49c4ce
Ravi Ranjan <Ravi.Ranjan@razorfish.com>
2015-09-07 20:26:01 +0530

    reset password page error message changes!

--- app/scripts/components/util/commons.directive.js
+++ app/scripts/components/util/commons.directive.js
@@ -17,3 +17,21 @@ angular.module('hillromvestApp')
     };
     return ddo;
   }]);
+
+  
+  
+  angular.module('hillromvestApp')
+  .directive('pwCheck', [function () {
+    return {
+      require: 'ngModel',
+      link: function (scope, elem, attrs, ctrl) {
+        var firstPassword = '#' + attrs.pwCheck;
+        elem.add(firstPassword).on('keyup', function () {
+          scope.$apply(function () {
+            var v = elem.val()===$(firstPassword).val();
+            ctrl.$setValidity('pwmatch', v);
+          });
+        });
+      }
+    }
+  }]);
