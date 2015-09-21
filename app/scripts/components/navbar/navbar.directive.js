a06ece78d866633ef1afdb2a17ff148f2f3d5468
Syed Ali <syedmohammed@neevtech.com>
2015-09-21 13:37:17 +0530

    Resolved PR comments

--- app/scripts/components/navbar/navbar.directive.js
+++ app/scripts/components/navbar/navbar.directive.js
@@ -21,14 +21,14 @@ angular.module('hillromvestApp')
   };
 }])
 
-.directive('activeLink',['location', function(location) {
+.directive('activeLink',['$location', function($location) {
   return {
     restrict: 'A',
     link: function (scope, element, attrs) {
       var clazz = attrs.activeLink;
       var path = attrs.href;
       path = path.substring(1); //hack because path does bot return including hashbang
-      scope.location = location;
+      scope.location = $location;
       scope.$watch('location.path()', function(newPath) {
         if (path === newPath) {
           element.addClass(clazz);
