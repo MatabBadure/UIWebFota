7a1f9f1fa0b950434c81a4ed57a8c71415e97fce
avnesh-neev <avnesh.nitk@gmail.com>
2015-09-25 12:26:16 +0530

    Animation with Nested view for Patient Profile

--- app/scripts/components/util/commons.directive.js
+++ app/scripts/components/util/commons.directive.js
@@ -51,22 +51,3 @@ angular.module('hillromvestApp')
     }; 
     return ddo;
   }])
-
-  .directive('activeTab', ['$location', function(location) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs, controller) {
-            var className = attrs.activeTab;
-            var path = attrs.href;
-            path = path.substring(1); //hack because path does bot return including hashbang
-            scope.location = location;
-            scope.$watch('location.path()', function(newPath) {
-                if (path === newPath) {
-                    element.addClass(className);
-                } else {
-                    element.removeClass(className);
-                }
-            });
-        }
-    };
-}]);
