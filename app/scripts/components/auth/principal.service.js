61c6ad5e1b516a7b19a260b82191e52520aa2b3f
Syed Ali <syedmohammed@neevtech.com>
2015-10-07 17:51:05 +0530

    ResponseError Interceptor Implementation

--- app/scripts/components/auth/principal.service.js
+++ app/scripts/components/auth/principal.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('hillromvestApp')
-    .factory('Principal',['$q', '$rootScope', 'Account', function Principal($q, $rootScope, Account) {
+    .factory('Principal',[function Principal() {
         var _identity,
             _authenticated = false;
 
@@ -36,17 +36,27 @@ angular.module('hillromvestApp')
                 _identity = identity;
                 _authenticated = identity !== null;
             },
-            identity: function (force) {
-                var deferred = $q.defer();
-
-                if (force === true) {
-                    _identity = undefined;
+            setIdentity: function (identity) {
+                   _identity = identity;
+            },
+            getIdentity: function() {
+                return _identity;
+            },
+            setAuthenticate: function(authenticate) {
+                _authenticated = authenticate;
+            }
+        };
+    }]).service('PrincipalService', ['Account', '$q', 'Principal', '$rootScope', function(Account, $q, Principal, $rootScope) {
+        this.identity = function(force) {
+             var deferred = $q.defer();
+              if (force === true) {
+                    Principal.setIdentity(undefined);
                 }
 
                 // check and see if we have retrieved the identity data from the server.
                 // if we have, reuse it by immediately resolving
-                if (angular.isDefined(_identity)) {
-                    deferred.resolve(_identity);
+                if (angular.isDefined(Principal.getIdentity())) {
+                    deferred.resolve(Principal.getIdentity());
 
                     return deferred.promise;
                 }
@@ -54,18 +64,17 @@ angular.module('hillromvestApp')
                 // retrieve the identity data from the server, update the identity object, and then resolve.
                 Account.get().$promise
                     .then(function (account) {
-                        _identity = account.data;
-                        _authenticated = true;
+                        Principal.setIdentity(account.data);
+                        Principal.setAuthenticate(true);
                         $rootScope.isAuthenticated = true;
-                        deferred.resolve(_identity);
+                        deferred.resolve(account.data);
                     })
                     .catch(function() {
-                        _identity = null;
-                        _authenticated = false;
+                        Principal.setIdentity(null);
+                        Principal.setAuthenticate(false);
                         $rootScope.isAuthenticated = false;
-                        deferred.resolve(_identity);
+                        deferred.resolve(null);
                     });
                 return deferred.promise;
-            }
-        };
+        }
     }]);
