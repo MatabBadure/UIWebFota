f1f5332f1479da51c50ce236d476bc7b63ef9414
Chinmayee <talk.is.mine@gmail.com>
2015-12-23 17:28:21 +0530

    Test code

--- app/scripts/main/main.controller.js
+++ app/scripts/main/main.controller.js
@@ -186,4 +186,89 @@ angular.module('hillromvestApp')
 				$scope.isUserChanged();
 			}
 		};
+		$scope.passwordStrength = function(){
+		    $scope.display_strength('passwordBox','passwordStrengthContainer','status');
+		};
+		$scope.display_strength = function(x, y, z) {
+		    if (!x || $scope.div(x) === "") {
+		      $scope.div(y).style.width = 0 + "%";
+		      $scope.div(z).innerHTML = "&nbsp:";
+		      return false;
+		    }
+		    var paswd = $scope.div(x);
+		    var stren = $scope.div(y);
+		    var stats = $scope.div(z);
+		    var years = 0;
+
+		    var regex = [
+		      /^[a-zA-Z]+$/g,
+		      /^[a-zA-Z0-9]+$/g,
+		      /^[a-zA-Z0-9\ \~\!\@\#\$\%\^\&\*\(\)]+$/g,
+		    ];
+
+		    var count = [42, 52, 63, 74];
+		    var color = ["red", "gold", "lime", "darkgreen"];
+		    var value = ["weak", "Good", "Strong", "Very Strong"];
+		    var index = 0;
+
+		    for (index = 0; index < regex.length; index++) {
+		      if (index > regex.length) {
+		        break;
+		      }
+		      if (regex[index].test(paswd.value)) {
+		        break;
+		      } else {
+		        continue;
+		      }
+
+		    }
+
+		    years = $scope.compute_strength(count[index], (paswd.value).length);
+
+		    if (years >= 1000 && !$scope.form.password.$error.pattern) {
+		      stats.innerHTML = value[3];
+		      stats.style.color = color[3];
+		    } else if (years >= 100 && years < 1000 && !$scope.form.password.$error.pattern) {
+		      stats.innerHTML = value[2];
+		      stats.style.color = color[2];
+		    } else if (years >= 10 && years < 100 && !$scope.form.password.$error.pattern) {
+		      stats.innerHTML = value[1];
+		      stats.style.color = color[1];
+		    } else {
+		      if($scope.form.password && $scope.form.password.$error.required){
+		        stats.innerHTML = '';
+		        stats.style.color = color[0];
+		      }else{
+		        stats.innerHTML = value[0];
+		        stats.style.color = color[0];
+		      } 
+		    }
+		    return false;
+		  };
+
+		$scope.compute_strength = function (x, y){
+		    if(!x){
+		        return -1;
+		    }
+		    var secondsInAYear = 31557600;
+		    var permutateRepeat = (Math.pow(x,y) - (x*$scope.factorial(y) - 1));
+		    var countManyYears = Math.floor(permutateRepeat/(1000000*secondsInAYear));
+
+		    return countManyYears;
+		};
+
+		$scope.factorial = function (x) {
+		    if(x<0){
+		        return false;
+		    }
+
+		    if(x == 0 ){
+		        return 1;
+		    }
+
+		    x = Math.floor(x);
+		    return (x*$scope.factorial(x-1));
+
+		};
+
     }]);
