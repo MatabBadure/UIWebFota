2fd1f96ebd083f4564c4abf272136fe9b5ca7fb3
Syed Ali <syedmohammed@neevtech.com>
2016-03-02 19:29:00 +0530

    In Progress

--- app/scripts/modules/common/console/surveyreport/controllers/surveyreport.controller.js
+++ app/scripts/modules/common/console/surveyreport/controllers/surveyreport.controller.js
@@ -1,7 +1,42 @@
 'use strict';
 angular.module('hillromvestApp')
-.controller('surveyReportController',['$scope', '$state', 'patientsurveyService', 'dateService',
-	function($scope, $state, patientsurveyService, dateService) {
+.controller('surveyReportController',['$scope', '$state', 'patientsurveyService', 'dateService', '$timeout',
+	function($scope, $state, patientsurveyService, dateService, $timeout) {
+		
+		$scope.calculateDateFromPicker = function(picker) {
+	    $scope.fromTimeStamp = new Date(picker.startDate._d).getTime();	      
+		  $scope.toTimeStamp = (new Date().getTime() < new Date(picker.endDate._d).getTime())? new Date().getTime() : new Date(picker.endDate._d).getTime();
+	    $scope.fromDate = dateService.getDateFromTimeStamp($scope.fromTimeStamp,patientDashboard.dateFormat,'/');
+	    $scope.toDate = dateService.getDateFromTimeStamp($scope.toTimeStamp,patientDashboard.dateFormat,'/');
+
+	    $scope.serverFromDate = dateService.getDateFromTimeStamp($scope.fromTimeStamp,patientDashboard.serverDateFormat,'-');
+	    $scope.serverToDate = dateService.getDateFromTimeStamp($scope.toTimeStamp,patientDashboard.serverDateFormat,'-');
+	    if ($scope.fromDate === $scope.toDate ) {
+	        $scope.fromTimeStamp = $scope.toTimeStamp;
+	    }	      
+	  };
+
+		$scope.opts = {
+			maxDate: new Date(),
+			format: patientDashboard.dateFormat,
+			eventHandlers: {'apply.daterangepicker': function(ev, picker) {  
+					$scope.calculateDateFromPicker(picker);     				
+					$scope.customDateRangeView();
+				},
+				'click.daterangepicker': function(ev, picker) {
+					$("#dp1cal").data('daterangepicker').setStartDate($scope.fromDate);
+					$("#dp1cal").data('daterangepicker').setEndDate($scope.toDate);
+				}
+			},
+			opens: 'left'
+		}
+
+		$scope.dates = {startDate: $scope.fromDate, endDate: $scope.toDate};
+
+		$scope.customDateRangeView = function(){
+			$scope.getSurveyReport($scope.surveyType, $scope.serverFromDate, $scope.serverToDate);
+		};
+		
 		$scope.init = function(){
 			if($state.current.name === 'adminSurveyReport' || $state.current.name === 'rcddminSurveyReport' || $state.current.name === 'associateSurveyReport'){
 				$scope.switchSurvey(1);
@@ -38,6 +73,11 @@ angular.module('hillromvestApp')
 				default:
 				break;
 			}
+		};
+
+		$scope.showComments = function(id){
+			console.log('ID: ', id);
+			$scope.showCommentModal = true;
 		};
 
 		$scope.init();
