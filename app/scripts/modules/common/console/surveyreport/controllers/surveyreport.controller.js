b0aace1917c099440825b57a80f4ce002acb5c1b
Syed Ali <syedmohammed@neevtech.com>
2016-03-02 14:31:14 +0530

    Survey Admin, RCAdmin, Associates Inprogress

--- app/scripts/modules/common/console/surveyreport/controllers/surveyreport.controller.js
+++ app/scripts/modules/common/console/surveyreport/controllers/surveyreport.controller.js
@@ -1,10 +1,42 @@
 'use strict';
 angular.module('hillromvestApp')
-.controller('surveyReportController',['$scope', '$state',
-	function($scope, $state) {
+.controller('surveyReportController',['$scope', '$state', 'patientsurveyService', 'dateService',
+	function($scope, $state, patientsurveyService, dateService) {
 		$scope.init = function(){
 			if($state.current.name === 'adminSurveyReport' || $state.current.name === 'rcddminSurveyReport' || $state.current.name === 'associateSurveyReport'){
-				console.log("It is survey report page for admin/rcadmin/associates");
+				$scope.switchSurvey(1);
+			}
+		};
+
+		$scope.getSurveyReport = function(type, fromDate, toDate){
+			patientsurveyService.getSurveyGridReport(type, fromDate, toDate).then(function(response){
+				console.log(response.data.surveyGridView);
+				$scope.count = response.data.count;
+				$scope.surveyGridView = response.data.surveyGridView;
+			}).catch(function(response){
+				console.log(response);
+			});
+		};
+
+		$scope.switchSurvey = function(type){
+			$scope.surveyType = type;
+			switch(type){
+				case 1: 
+					var fromDate = dateService.getDateFromTimeStamp(new Date(),patientDashboard.serverDateFormat,'-');
+					$scope.getSurveyReport(type, fromDate, fromDate);
+				break;
+
+				case 2:
+					var fromDate = dateService.getDateFromTimeStamp(new Date(),patientDashboard.serverDateFormat,'-');
+					$scope.getSurveyReport(type, fromDate, fromDate);
+				break;
+
+				case 3:
+					var fromDate = dateService.getDateFromTimeStamp(new Date(),patientDashboard.serverDateFormat,'-');
+					$scope.getSurveyReport(type, fromDate, fromDate);
+				break;
+				default:
+				break;
 			}
 		};
 
