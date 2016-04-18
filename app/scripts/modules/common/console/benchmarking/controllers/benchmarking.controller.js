c7ee6f493635a5cbb14192a1bf26c37dc1d13a1c
Chinmayee <talk.is.mine@gmail.com>
2016-04-18 16:06:46 +0530

    make the width of the lines less in ipad

--- app/scripts/modules/common/console/benchmarking/controllers/benchmarking.controller.js
+++ app/scripts/modules/common/console/benchmarking/controllers/benchmarking.controller.js
@@ -3,6 +3,7 @@ angular.module('hillromvestApp')
 .controller('benchmarkingController', ['$scope', 'addressService', 'notyService', '$rootScope', 'benchmarkingService', 'dateService', 'exportutilService', 'pdfServiceConstants', '$state', 'StorageService', 'loginConstants',
 	function($scope, addressService, notyService, $rootScope, benchmarkingService, dateService, exportutilService, pdfServiceConstants, $state, StorageService, loginConstants) {
 
+		$scope.pointWidth = $rootScope.isIOS()  ? 50 : 30;
 		$scope.calculateDateFromPicker = function(picker) {
 	    $scope.fromTimeStamp = new Date(picker.startDate._d).getTime();	      
 		  $scope.toTimeStamp = (new Date().getTime() < new Date(picker.endDate._d).getTime())? new Date().getTime() : new Date(picker.endDate._d).getTime();
@@ -357,8 +358,8 @@ angular.module('hillromvestApp')
 		    },
 		    plotOptions: {
 		      series: {
-		      	showInLegend: false/*,
-		      	pointWidth: 50*/
+		      	showInLegend: false,
+		      	pointWidth: $scope.pointWidth
 		      }
 		    },
 				tooltip: {
@@ -577,8 +578,8 @@ angular.module('hillromvestApp')
 		    	},
 		    	plotOptions: {
 		      	series: {
-		      		showInLegend: false/*,
-		      		pointWidth: 50*/
+		      		showInLegend: false,
+		      		pointWidth: $scope.pointWidth
 		      	},
 		      	column: {
 							stacking: ($scope.xaxis === 'both') ? 'normal': false,
