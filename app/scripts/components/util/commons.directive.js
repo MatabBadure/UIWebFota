643e49d8a74749feed6b4ff82c31e084d3d8451e
Syed Ali <syedmohammed@neevtech.com>
2016-04-28 16:14:35 +0530

    CSS fix and Directive Fix

--- app/scripts/components/util/commons.directive.js
+++ app/scripts/components/util/commons.directive.js
@@ -56,31 +56,31 @@ angular.module('hillromvestApp')
   .directive('maxFloat', ['$parse', function ($parse) {
   return {
     require: 'ngModel',
-    link: function(scope, elem, attrs, ngModel) {     
-      scope.$watch(attrs.maxFloat, function (newValue, oldValue){          
+    link: function(scope, elem, attrs, ngModel) {
+      scope.$watch(attrs.maxFloat, function (newValue, oldValue){
           var digiTed = "";
           if(newValue !== undefined){
             try{
               digiTed = newValue.replace(/[^0-9\.]/g, ''); 
             }catch(e){
               digiTed = newValue;
-            }                       
-            digiTed = (digiTed === ".") ? "0.": (digiTed > 0 ? (digiTed.toString().indexOf(".") === -1 ? parseFloat(digiTed).toString() : digiTed) : (digiTed.toString().indexOf(".") === -1 && digiTed == 0)? 0 : digiTed );            
-            //  get only two digits after decimal, if available                             
-            digiTed = (digiTed && digiTed.toString().indexOf(".") !== -1) ? ((digiTed.toString().split(".")[1]).toString().length > 2? digiTed.substring(0, digiTed.length-1) : digiTed ): digiTed;            
-            //check for max value  
+            }
+            digiTed = (digiTed === ".") ? "0.": (digiTed > 0 ? (digiTed.toString().indexOf(".") === -1 ? parseFloat(digiTed).toString() : digiTed) : (digiTed.toString().indexOf(".") === -1 && digiTed == 0)? 0 : digiTed );
+            //  get only two digits after decimal, if available
+            digiTed = (digiTed && digiTed.toString().indexOf(".") !== -1) ? ((digiTed.toString().split(".")[1]).toString().length > 2? digiTed.substring(0, digiTed.length-1) : digiTed ): digiTed;
+            //check for max value
             if(attrs.maxLimit){
-              digiTed = (digiTed && digiTed.toString().length > 0) ? ((digiTed <= parseInt(attrs.maxLimit) )? digiTed : oldValue ) : (digiTed === 0 ? 0: null) ;
+              digiTed = (digiTed && digiTed.toString().length > 0) ? ((digiTed <= parseInt(attrs.maxLimit) )? digiTed : oldValue ) : (digiTed === 0 ? 0: null);
             }else if(attrs.maxBound){
-              digiTed = (digiTed && digiTed.toString().length > 0) ? ((digiTed < parseInt(attrs.maxBound) )? digiTed : oldValue ) : (digiTed === 0 ? 0: null) ;
-            }                                    
-         }           
-          try{
-            digiTed = digiTed.toString().replace(/^0+(?!\.|$)/, '');
-          }catch(e){
-            digiTed = digiTed.toString();
-          }          
-          $parse(attrs.maxFloat).assign(scope,digiTed.toString());             
+              digiTed = (digiTed && digiTed.toString().length > 0) ? ((digiTed < parseInt(attrs.maxBound) )? digiTed : oldValue ) : (digiTed === 0 ? 0: null);
+            }
+            try{
+              digiTed = digiTed.toString().replace(/^0+(?!\.|$)/, '');
+            }catch(e){
+              digiTed = digiTed.toString();
+            }
+            $parse(attrs.maxFloat).assign(scope,digiTed.toString());
+         }
       })
     }
   };
