d3e75978f833b2c43a01a2e2f75ca9c7d10d2489
zebatahseen <Zeba.Tahseen@Lnttechservices.com>
2017-01-19 16:38:30 +0530

    View PDF for devices and browsers

--- app/scripts/modules/common/console/announcements/services/announcement.service.js
+++ app/scripts/modules/common/console/announcements/services/announcement.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('hillromvestApp')
-  .factory('announcementservice',['$http', 'headerService', 'URL', function ($http, headerService, URL) {
+  .factory('announcementservice',['$http', 'headerService', 'URL', 'deviceDetector', function ($http, headerService, URL, deviceDetector) {
     return {
 
 deleteAnnouncement : function(id){
@@ -69,7 +69,6 @@ return $http.get(url, {
       },
       ListAnnouncement : function(pageNumber,perPage,sortOption,usertype,userId){
            var url = URL.listAnnouncements.replace('PAGE',pageNumber).replace('PER_PAGE',perPage).replace('SORT_OPTION',sortOption).replace('USER_TYPE',usertype).replace('USERID',userId);
-        console.log(url);
         return $http.get(url, {
           headers: headerService.getHeader()
         }).success(function(response) {
@@ -77,18 +76,17 @@ return $http.get(url, {
         });
       }, 
       ListAnnouncementPatient: function(pageNumber,perPage,sortOption,usertype,userId){
-           var url = URL.listAnnouncementsPatient.replace('PAGE',pageNumber).replace('PER_PAGE',perPage).replace('SORT_OPTION',sortOption).replace('USER_TYPE',usertype).replace('USERID',userId);
-        console.log(url);
-        return $http.get(url, {
+           var url = URL.listAnnouncementsPatient.replace('PAGE',pageNumber).replace('PER_PAGE',perPage).replace('SORT_OPTION',sortOption).replace('USER_TYPE',usertype).replace('USERID',userId);        return $http.get(url, {
           headers: headerService.getHeader()
         }).success(function(response) {
           return response;
         });
       }, 
+
       DownloadAsPDF : function(filename){
         var windowReference = "";
         var userAgent = window.navigator.userAgent;
-        if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.indexOf("Safari")!=-1) {
+        if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || deviceDetector.browser == 'safari') {
         windowReference = window.open();
       }
         var url = URL.downloadPdf.replace('FILE_NAME',filename);
@@ -96,34 +94,20 @@ return $http.get(url, {
           headers: headerService.getHeaderForPdf(),
           responseType: 'arraybuffer'
         }).success(function(response) {
-          //var fileName = "test.pdf";
-          //var a = document.createElement("a");
-          //document.body.appendChild(a);
-/*          
-          var file = new Blob([response], {type: 'application/pdf'});
-          var fileURL = URL.createObjectURL(file);
-          window.open(fileURL);*/
-          //a.href = fileURL;
-         //a.download = fileName;
-         //a.click();
-
-  
          var URL = window.URL || window.webkitURL;
                 var byteArray = new Uint8Array(response);
                 var blob = new Blob([byteArray], { type: 'application/pdf' });
                 if ((window.navigator && window.navigator.msSaveOrOpenBlob) || (userAgent.indexOf("android") > -1)){
                     window.navigator.msSaveOrOpenBlob(blob);
                 }
-                else if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.indexOf("Safari")!=-1) {
+                else if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || deviceDetector.browser == 'safari') {
                  //Safari & Opera iOS
-
-                
                   var objectUrl = URL.createObjectURL(blob);
 
                      windowReference.location = objectUrl; 
                 }
-                else {
 
+                else {
                     var objectUrl = URL.createObjectURL(blob);
                     window.open(objectUrl);
                     /* windowReference.location = objectUrl; */
@@ -131,6 +115,7 @@ return $http.get(url, {
         });
     
       } 
+
       
         };
 }]);
