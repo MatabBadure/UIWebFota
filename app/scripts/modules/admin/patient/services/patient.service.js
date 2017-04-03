8eb2d3b4b9530db525e78d8ee18ee893eacddb49
zebatahseen <zeba.tahseen@lnttechservices.com>
2017-04-03 15:23:10 +0530

    HILL-2133_implementation

--- app/scripts/modules/admin/patient/services/patient.service.js
+++ app/scripts/modules/admin/patient/services/patient.service.js
@@ -264,7 +264,18 @@ angular.module('hillromvestApp')
         });
       },
 
+ // get reset adhrence history starts here
 
+      getAdherenceScoreResetHistory: function(id,pageNumber,perPage) {
+        var url = URL.getAdherenceScoreResetHistory.replace('ID',id).replace('PAGE',pageNumber).replace('PER_PAGE',perPage).replace('DEVICETYPE',localStorage.getItem('deviceTypeforBothIcon'));
+        return $http.get(url, {
+          headers: headerService.getHeader()
+        }).success(function(response) {
+          return response;
+        });
+      },
+     
+     // get reset adhrence history ends here
       addDevice: function(id, data, deviceType) {
        
           var url = URL.addDevice.replace('PATIENTID', id).replace('DEVICETYPE',deviceType);
