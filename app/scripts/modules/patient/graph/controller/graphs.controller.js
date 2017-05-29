34ea93651a211970d943863355586ec6e72a7277
zebatahseen <Zeba.Tahseen@Lnttechservices.com>
2017-05-29 17:59:02 +0530

    custoem date

--- app/scripts/modules/patient/graph/controller/graphs.controller.js
+++ app/scripts/modules/patient/graph/controller/graphs.controller.js
@@ -2174,12 +2174,14 @@ angular.module('hillromvestApp')
           $scope.transmissionDate = (formattedTransmissionDate && formattedTransmissionDate.indexOf(" "))? formattedTransmissionDate.split(" ")[0] : null; 
           $scope.hasTransmissionDateforCostomrange = dateService.getDateFromTimeStamp(response.data.firstTransmissionDate,patientDashboard.dateFormat,'/'); 
           //alert($scope.hasTransmissionDateforCostomrange); 
-          $scope.opts = {
+        setTimeout(function(){ 
+              $scope.opts = {
           minDate: $scope.hasTransmissionDateforCostomrange
         };
         $scope.dateOpts = {
           minDate: $scope.hasTransmissionDateforCostomrange
-          };      
+          };         
+          }, 100);     
         }
       });
     };
