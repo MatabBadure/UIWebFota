ccfd0ad5af43f189be3c16b6c265e373f5cb609f
zebatahseen <Zeba.Tahseen@Lnttechservices.com>
2017-08-07 17:20:03 +0530

    Initial changes for optimus sandbox

--- app/scripts/modules/admin/profile/service/charger.service.js
+++ app/scripts/modules/admin/profile/service/charger.service.js
@@ -3,7 +3,7 @@ angular.module('hillromvestApp')
   .factory('chargerservice', ['$http', 'headerService', 'URL' ,
     function($http, headerService, URL) {
     return {
-      getListDataFromService: function() {
+      getListDataForCharger: function() {
         var url = URL.chargerDataList;
         return $http.get(url, {
           headers: headerService.getHeader()
@@ -12,13 +12,32 @@ angular.module('hillromvestApp')
         });
       },
 
-       getClickedDataFromService: function(id) {
+       getClickedDataForCharger: function(id) {
         var url = URL.clickedListData.replace('ID',id);
         return $http.get(url, {
           headers: headerService.getHeader()
         }).success(function(response) {
           return response;
         });
-      }
-    };
+      },
+   
+
+    getListDataForOptimus: function(page,perpage){
+      var url = URL.optimusdevicedatalist.replace('PAGE',page).replace('PER_PAGE',perpage);
+        return $http.get(url, {
+          headers: headerService.getHeader()
+        }).success(function(response) {
+          return response;
+        });
+    },
+
+    getClickedDataForOptimus: function(id){
+      var url = URL.optimusDeviceData.replace('ID',id);
+      return $http.get(url, {
+          headers: headerService.getHeader()
+        }).success(function(response) {
+          return response;
+        });
+    }
+  };
    }]);
