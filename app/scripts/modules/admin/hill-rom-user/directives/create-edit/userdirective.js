55658be13bcc41070dc36c3a5691ca028523c041
Basheer Usman <zeba.tahseen@lnttechservices.com>
2017-11-16 21:37:34 +0530

    Reset Password

--- app/scripts/modules/admin/hill-rom-user/directives/create-edit/userdirective.js
+++ app/scripts/modules/admin/hill-rom-user/directives/create-edit/userdirective.js
@@ -19,14 +19,14 @@ angular.module('hillromvestApp')
       },
       controller: ['$scope', 'notyService', '$state', 'UserService', 'StorageService', 'Auth', '$rootScope', function ($scope, notyService, $state, UserService, StorageService, Auth, $rootScope) {
         $scope.currentUserRole = StorageService.get('logged').role;
-
-     
-        $scope.init = function(){
+        $scope.init = function () {
+    
           $scope.nonHillRomUsers = ['PATIENT', 'HCP', 'CLINIC_ADMIN', 'CARE_GIVER'];
-          if($state.current.name === 'hillRomUserEdit' || $state.current.name === 'rcadmin-hillRomUserEdit' || $state.current.name === 'customerserviceHillRomUserView'){
+
+          if ($state.current.name === 'hillRomUserEdit' || $state.current.name === 'rcadmin-hillRomUserEdit' || $state.current.name === 'customerserviceHillRomUserView') {
 
             $scope.loggedUserId = StorageService.get('logged').userId;
-            }
+          }
 
         };
         $scope.open = function () {
@@ -43,10 +43,8 @@ angular.module('hillromvestApp')
         };
 
         $scope.validateSuperAdmin = function () {
-    
-          $scope.isUser = ($scope.user.role === 'CARE_GIVER' || $scope.user.role === 'PATIENT' || $scope.user.role === 'HCP') ?false: true  ;
-      
-          $scope.isSuperadmin = ($scope.user.role === 'ADMIN' && $scope.currentUserRole === 'ACCT_SERVICES') ? true : false;
+           $scope.isUser =   ($scope.user.role === 'CARE_GIVER' || $scope.user.role === 'PATIENT' || $scope.user.role === 'HCP' || $scope.user.role === 'CLINIC_ADMIN') ? true : false;
+            $scope.isSuperadmin = ($scope.user.role === 'ADMIN' && $scope.currentUserRole === 'ACCT_SERVICES') ? true : false;
           if ($scope.userStatus.editMode && !($scope.userStatus.role === roleEnum.ADMIN || $scope.userStatus.role === roleEnum.ACCT_SERVICES)) {
             return true;
           }
@@ -66,7 +64,7 @@ angular.module('hillromvestApp')
           }
           if ($scope.userStatus.editMode) {
             //will be removed when we support multiple role
-            if($scope.user.authorities){
+            if ($scope.user.authorities) {
               delete $scope.user.authorities;
             }
             $scope.editUser($scope.user);
@@ -75,14 +73,15 @@ angular.module('hillromvestApp')
           }
         };
 
-        $scope.$on('getUserDetail', function (event, args){
-          if(args.user.email === StorageService.get('logged').userEmail){
+        $scope.$on('getUserDetail', function (event, args) {
+          
+          if (args.user.email === StorageService.get('logged').userEmail) {
             $scope.selectedSelf = true;
             $scope.myHRID = args.user.hillromId;
           }
         });
 
-        $scope.newUser = function(data) {
+        $scope.newUser = function (data) {
           UserService.createUser(data).then(function (response) {
             $scope.userStatus.isMessage = true;
             $scope.userStatus.message = 'User created successfully';
@@ -92,7 +91,7 @@ angular.module('hillromvestApp')
             $scope.userStatus.isMessage = true;
             if (response.data.message !== undefined) {
               $scope.userStatus.message = response.data.message;
-            }else if(response.data.ERROR !== undefined){
+            } else if (response.data.ERROR !== undefined) {
               $scope.userStatus.message = response.data.ERROR;
             } else {
               $scope.userStatus.message = 'Error occured! Please try again';
@@ -101,23 +100,23 @@ angular.module('hillromvestApp')
           });
         };
 
-        $scope.editUser = function(data) {
+        $scope.editUser = function (data) {
           UserService.editUser(data).then(function (response) {
             $scope.userStatus.isMessage = true;
             $scope.userStatus.message = response.data.message;
             notyService.showMessage($scope.userStatus.message, 'success');
-            if($scope.selectedSelf && (StorageService.get('logged').userEmail !== response.data.user.email || $scope.myHRID !== response.data.user.hillromId) ){
+            if ($scope.selectedSelf && (StorageService.get('logged').userEmail !== response.data.user.email || $scope.myHRID !== response.data.user.hillromId)) {
               $rootScope.userRole = null;
               Auth.logout();
               $state.go('login');
-            }else{
+            } else {
               $scope.reset();
             }
           }).catch(function (response) {
             $scope.userStatus.isMessage = true;
             if (response.data.message !== undefined) {
               $scope.userStatus.message = response.data.message;
-            }else if(response.data.ERROR !== undefined){
+            } else if (response.data.ERROR !== undefined) {
               $scope.userStatus.message = response.data.ERROR;
             } else {
               $scope.userStatus.message = 'Error occured! Please try again';
@@ -142,9 +141,9 @@ angular.module('hillromvestApp')
           }).catch(function (response) {
             $scope.showModal = false;
             $scope.userStatus.isMessage = true;
-            if(response.data.message !== undefined) {
+            if (response.data.message !== undefined) {
               $scope.userStatus.message = response.data.message;
-            } else if(response.data.ERROR !== undefined){
+            } else if (response.data.ERROR !== undefined) {
               $scope.userStatus.message = response.data.ERROR;
             } else {
               $scope.userStatus.message = 'Error occured! Please try again';
@@ -153,45 +152,45 @@ angular.module('hillromvestApp')
           });
         };
 
-        $scope.cancel = function(){
+        $scope.cancel = function () {
           $scope.reset();
         };
 
-        $scope.resendActivationLink = function(){
-          UserService.resendActivationLink($scope.user.id).then(function(response){
+        $scope.resendActivationLink = function () {
+          UserService.resendActivationLink($scope.user.id).then(function (response) {
             $scope.isDisableResendButton = true;
-            notyService.showMessage(response.data.message, 'success'); 
-          }).catch(function(response){
+            notyService.showMessage(response.data.message, 'success');
+          }).catch(function (response) {
             notyService.showError(response);
           });
         };
 
-        $scope.activateUser = function(){
+        $scope.activateUser = function () {
           $scope.showActivateModal = false;
-          UserService.reactivateUser($scope.user.id).then(function(response){
-           notyService.showMessage(response.data.message, 'success');
-           if($scope.currentUserRole == 'ADMIN'){
-            $state.go('hillRomUser');
-          }
-          else if($scope.currentUserRole == 'ACCT_SERVICES'){
-             $state.go('rcadmin-hillRomUser');
-          }
-          }).catch(function(response){
-           notyService.showError(response);
+          UserService.reactivateUser($scope.user.id).then(function (response) {
+            notyService.showMessage(response.data.message, 'success');
+            if ($scope.currentUserRole == 'ADMIN') {
+              $state.go('hillRomUser');
+            }
+            else if ($scope.currentUserRole == 'ACCT_SERVICES') {
+              $state.go('rcadmin-hillRomUser');
+            }
+          }).catch(function (response) {
+            notyService.showError(response);
           });
         };
 
-        $scope.reset = function(){
+        $scope.reset = function () {
           $scope.user = {};
           $scope.userStatus.isCreate = false;
           $scope.userStatus.editMode = false;
           $scope.form.$setPristine();
           $scope.submitted = false;
-          if($scope.currentUserRole == 'ADMIN'){
+          if ($scope.currentUserRole == 'ADMIN') {
             $state.go('hillRomUser');
           }
-          else if($scope.currentUserRole == 'ACCT_SERVICES'){
-             $state.go('rcadmin-hillRomUser');
+          else if ($scope.currentUserRole == 'ACCT_SERVICES') {
+            $state.go('rcadmin-hillRomUser');
           }
         };
 
@@ -210,15 +209,16 @@ angular.module('hillromvestApp')
           });
         };
 
-        $scope.showUpdateModal = function(){
+        $scope.showUpdateModal = function () {
           $scope.submitted = true;
-          if($scope.form.$invalid){
+          if ($scope.form.$invalid) {
             return false;
           }
           $scope.updateModal = true;
         };
         $scope.showResetModel = function () {
-
+         
+        
           if ($scope.form.$invalid) {
             return true;
           } else {
@@ -227,7 +227,7 @@ angular.module('hillromvestApp')
 
         }
 
-     
+
         $scope.init();
       }]
     };
