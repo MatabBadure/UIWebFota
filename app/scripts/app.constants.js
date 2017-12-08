6c229fb4f0040615cd09b67eadc4b5617068249e
Basheer Usman <zeba.tahseen@lnttechservices.com>
2017-12-08 22:43:41 +0530

    alphanumeric zipcode

--- app/scripts/app.constants.js
+++ app/scripts/app.constants.js
@@ -176,7 +176,7 @@ angular.module('hillromvestApp')
 	'clinicAdvancedfilter' : 'api/clinics/advanced/search?&page=PAGE&per_page=PER_PAGE&sort_by=SORT_OPTION&asc=true',
 	'availableStatesAdv':'/api/availableStatesAdv',
 	'cityByCountryAndState':'/api/cityByCountryAndState',
-	'stateByCountryCode':'/api/stateByCountryCode?country=COUNTRY',
+	'stateByCountryCode':'/api/stateByCountryCode',
 	'patientAdvancedSearch' : 'api/advancedSearch/patient?&page=PAGE&per_page=PER_PAGE&sort_by=SORT_OPTION&asc=true',
 	'matchingDiagnosticList' : 'api/patient/diagnosis?searchString=SEARCH_STRING',
 	'timezoneList' : 'api/user/timezone',
