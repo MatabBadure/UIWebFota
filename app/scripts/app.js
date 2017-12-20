3d9a63ae0255941391f5a5ddb3ca5380180b3073
zebatahseen <Zeba.Tahseen@Lnttechservices.com>
2017-12-20 11:54:17 +0530

    Cache issue

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -96,7 +96,7 @@ angular.module('hillromvestApp',
 
     //Cache everything except rest api requests
 
-    httpRequestInterceptorCacheBusterProvider.setMatchlist([/.*api.*/, /.*protected.*/, /.*.json.*/, /.*scripts.*/], true, 'Sprint_5_2017_1');
+    httpRequestInterceptorCacheBusterProvider.setMatchlist([/.*api.*/, /.*protected.*/, /.*.json.*/, /.*scripts.*/, /.*main.min.js.*/,/.*main.min.css.*/], true, 'Sprint_5_2017_2');
 
 
 
